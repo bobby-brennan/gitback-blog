6c2a4d36144a69ae33ccdd45e32d5b2b0015fcaa
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-09-24 18:00:16 +0000

    add article sort

--- articles.js
+++ articles.js
@@ -4,6 +4,13 @@
       extension: 'md'
     }
   },
+  sort: function(a1, a2) {
+    var d1 = new Date(a1.date);
+    var d2 = new Date(a2.date);
+    return d1 > d2 ? -1
+         : d1 < d2 ? 1
+         : 0;
+  },
   access: {
     get: 'all'
   }
